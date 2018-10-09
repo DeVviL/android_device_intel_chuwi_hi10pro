83a2f5a0cc5568d7b43beca42e3a53c35489fb22
DeVviL <deuvil.den@gmail.com>
2018-10-09 12:54:06 +0300

    upd cm-14.1

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,7 +2,16 @@
 cd ../../../..
 cd frameworks/av
 patch -p1 < ../../device/intel/chuwi_hi10pro/patches/patches_intel/frameworks_av.patch
+cd ..
+cd native
+patch -p1 < ../../device/intel/chuwi_hi10pro/patches/0005-_ZN7android13GraphicBufferC1Ejjij-symbol-fix-on-fram.patch
 cd ../..
 cd packages/apps/Settings
 patch -p1 < ../../../device/intel/chuwi_hi10pro/patches/0005-add-author-info-in-device-info.patch
 cd ../../..
+cd system/core
+patch -p1 < ../../device/intel/chuwi_hi10pro/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+cd ..
+cd sepolicy
+patch -p1 < ../../device/intel/chuwi_hi10pro/patches/system_sepolicy.patch
+cd ../..
